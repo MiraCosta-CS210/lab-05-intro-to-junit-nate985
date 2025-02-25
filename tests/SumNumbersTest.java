6faa2a9ababb775ceb79eee6286b0489cec546ad
nate985 <89491528+nate985@users.noreply.github.com>
2025-02-25 09:15:18 -0800

    Ready to grade!

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SumNumbersTest {
+    @org.junit.jupiter.api.Test
+    void sumIsEvenTest()
+    {
+        SumNumbers sumNumbers = new SumNumbers();
+
+        //TEST SCENARIO 1
+        //INPUTS: 1, 1
+        //EXPECTED OUTPUT: 1 (which represents "true" - meaning the sum of the two numbers is even)
+        int result = sumNumbers.sumIsEven(1,1);
+        assertEquals(1, result);
+
+        //TEST SCENARIO 2
+        //INPUTS: 1, 2
+        //EXPECTED OUTPUT: 0 (which represents "false" - meaning the sum of the two numbers is odd)
+        int result2 = sumNumbers.sumIsEven(1,2);
+        assertEquals(0, result2);
+    }
 }
--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TipCalculatorTest {
+    @org.junit.jupiter.api.Test
+    void addTest() {
+        TipCalculator tipCalculator = new TipCalculator();
+
+        //TEST SCENARIO 1
+        //INPUTS: $100.00 bill, 20% tip
+        //EXPECTED OUTPUT: $20.00
+        double result = tipCalculator.calculateTip(100.00, 20);
+        assertEquals(20.00, result);
+
+        //TEST SCENARIO 2
+        //INPUTS: $150.25 bill, 15% tip
+        //EXPECTED OUTPUT: $22.54
+        double result2 = tipCalculator.calculateTip(150.25, 15);
+        assertEquals(22.54, result2);
+    }
 }
